64b88d29b5fe107860c8681de1b9f176487935b9
sattatrapy <fedina.nazar@gmail.com>
2020-07-07 15:27:24 +0300

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    infinite: true,
+    arrows: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    cssEase: "linear",
+    dots: true,
+  });
+
+});
